4fa1a3e5438116db865afef2689ca388fc7a6018
Karel Ježík <jezikkarel@gmail.com>
2023-02-06 19:31:22 +0100

    badgefix

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,12 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <a
+      id="badge"
+      target="_blank"
+      href="https://github.com/Keristrasz/Drum-Machine---React"
+    >
+      <div>Code on GitHub</div>
+    </a>
   </React.StrictMode>
 );
